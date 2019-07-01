ab233c68885d073e6930f1d57339fb178ff33fbb
sgk2425 <sgk2425@gmail.com>
2019-07-01 07:20:48 +0530

    updated utils

--- src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
+++ src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
@@ -1,33 +1,28 @@
 package com.actitime.loginlogoututils;
 
-import java.io.IOException;
-
 import org.openqa.selenium.support.PageFactory;
 
 import com.actitime.loginlogoutobjects.LoginLogoutObjects;
 import com.automation.base.BaseClass;
 import com.automation.utils.SeleniumCore;
-import com.automation.utils.SeleniumUtils;
 
 public class LoginLogoutUtils extends BaseClass{
 	
-	public SeleniumCore seleniumcore;
-	public SeleniumUtils seleniumutils;
-	public LoginLogoutObjects loginlogoutobjects;
+	private SeleniumCore seleniumcore;
+	private LoginLogoutObjects loginlogoutobjects;
 
 	public LoginLogoutUtils() {
 		seleniumcore=PageFactory.initElements(driver, SeleniumCore.class);
-		seleniumutils=PageFactory.initElements(driver, SeleniumUtils.class);
 		loginlogoutobjects=PageFactory.initElements(driver, LoginLogoutObjects.class);
 	}
 
-	public void loginCredentials(String username,String password) throws Exception {
+	public void loginCredentials(String username,String password) {
 		seleniumcore.enterTextBoxValue(loginlogoutobjects.username, username);
 		seleniumcore.enterTextBoxValue(loginlogoutobjects.password, password);
 		seleniumcore.clickonElement(loginlogoutobjects.loginButton, "Login");
 	}
 	
-	public void logoutApplication() throws IOException{
+	public void logoutApplication() {
 		seleniumcore.clickonElement(loginlogoutobjects.logoutButton, "Login");
 	}
 }
