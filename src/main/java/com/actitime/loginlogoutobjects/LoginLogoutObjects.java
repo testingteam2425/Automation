46fd99da518e7413154dec130c6366a505b5f8d3
sgk2425 <sgk2425@gmail.com>
2019-07-01 07:19:32 +0530

    Added objects

--- src/main/java/com/actitime/loginlogoutobjects/LoginLogoutObjects.java
+++ src/main/java/com/actitime/loginlogoutobjects/LoginLogoutObjects.java
@@ -7,17 +7,20 @@ public class LoginLogoutObjects {
 	
 	@FindBy(name="username")
 	public WebElement username;
-
+	
 	@FindBy(name="pwd")
 	public WebElement password;
 	
 	@FindBy(id="loginButton")
 	public WebElement loginButton;
-	 
+	
 	@FindBy(id="")
 	public WebElement logoutButton;
 	
 	@FindBy(id="")
 	public WebElement forgetPassword;
+	
+	@FindBy(xpath="//span[text()='Username or Password is invalid. Please try again.']")
+	public WebElement errormessgae;
 
 }
