e770e84c9c182108ce244baa2733f77fb94083dd
talla <talla@DESKTOP-MUEO3J6>
2020-05-05 07:23:09 +0530

    Commiting latest loginlogout class

--- src/main/java/com/actitime/loginlogoutobjects/LoginLogoutObjects.java
+++ src/main/java/com/actitime/loginlogoutobjects/LoginLogoutObjects.java
@@ -1,37 +1,6 @@
 package com.actitime.loginlogoutobjects;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
 public class LoginLogoutObjects {
 	
-	@FindBy(name="username")
-	public WebElement username;
 	
-	@FindBy(name="pwd")
-	public WebElement password;
-	
-	@FindBy(id="loginButton")
-	public WebElement loginButton;
-	
-	
-	
-	@FindBy(id="Dashboard")
-	public WebElement homepage;
-	
-	@FindBy(id="")
-	public WebElement logoutButton;
-	
-	@FindBy(id="")
-	public WebElement forgetPassword;
-	
-	@FindBy(id="")
-	public WebElement VerifyHeader;
-	
-	@FindBy(xpath="//span[text()='Username or Password is invalid. Please try again.']")
-	public WebElement errormessgae;
-	
-	@FindBy(id="")
-	public WebElement VerifyFooter;
-
 }
