63eea738643c27a4783c929ca6aae908c7e53b63
talla <talla@DESKTOP-MUEO3J6>
2020-05-05 07:31:48 +0530

    Updating both loginlogout utils and scripts

--- src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
+++ src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
@@ -1,30 +1,8 @@
 package com.actitime.loginlogoututils;
 
-import java.io.IOException;
-
-import org.openqa.selenium.support.PageFactory;
-
-import com.actitime.loginlogoutobjects.LoginLogoutObjects;
 import com.automation.base.BaseClass;
-import com.automation.utils.SeleniumCore;
 
 public class LoginLogoutUtils extends BaseClass{
 	
-	private SeleniumCore seleniumcore;
-	private LoginLogoutObjects loginlogoutobjects;
-
-	public LoginLogoutUtils() {
-		seleniumcore=PageFactory.initElements(driver, SeleniumCore.class);
-		loginlogoutobjects=PageFactory.initElements(driver, LoginLogoutObjects.class);
-	}
-
-	public void loginCredentials(String username,String password) throws Exception {
-		seleniumcore.enterTextBoxValue(loginlogoutobjects.username, username);
-		seleniumcore.enterTextBoxValue(loginlogoutobjects.password, password);
-		seleniumcore.clickonElement(loginlogoutobjects.loginButton, "Login");
-	}
 	
-	public void logoutApplication() throws IOException {
-		seleniumcore.clickonElement(loginlogoutobjects.logoutButton, "Login");
-	}
 }
