b9c6802419ff29e5a1d5bddda9219c3f74b67890
talla <talla@DESKTOP-MUEO3J6>
2020-05-05 07:35:30 +0530

    Updated all classes

--- src/main/java/com/actitime/reportsobjects/ReportsDashboardObjects.java
+++ src/main/java/com/actitime/reportsobjects/ReportsDashboardObjects.java
@@ -1,17 +1,6 @@
 package com.actitime.reportsobjects;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
 public class ReportsDashboardObjects {
-	
-	@FindBy(name="username")
-	public WebElement username;
-	
-		
-	@FindBy(name="username")
-	public WebElement userna;
 	
 	
 }
-
--- src/main/java/com/actitime/tasksutils/Taskutils.java
+++ src/main/java/com/actitime/tasksutils/Taskutils.java
@@ -1,38 +1,9 @@
 package com.actitime.tasksutils;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
-
-import com.actitime.tasksobjects.TaskObjects;
 import com.automation.base.BaseClass;
-import com.automation.utils.SeleniumCore;
-import com.automation.utils.SeleniumUtils;
 
 public class Taskutils extends BaseClass {
 
-	public SeleniumCore seleniumcore;
-	public SeleniumUtils seleniumutils;
-
-	public TaskObjects tasksobjects;
-
-	public Taskutils() {
-		seleniumcore = PageFactory.initElements(driver, SeleniumCore.class);
-		seleniumutils = PageFactory.initElements(driver, SeleniumUtils.class);
-		tasksobjects = PageFactory.initElements(driver, TaskObjects.class);
-
-	}
-
-	public void tasksInput() throws Exception {
-		/*
-		 * seleniumcore.enterText(tasksobjects.USERXPATH,username);
-		 * seleniumcore.enterText(tasksobjects.PASSXPATH,passwd);
-		 * seleniumcore.getClick(tasksobjects.LOGINBTN);
-		 */
-		seleniumcore.elementNotClickable(tasksobjects.HeaderTasks);
-		seleniumcore.clickonElement(tasksobjects.AddNewTaskButton, "clicking the button");
-		seleniumcore.clickonElement(tasksobjects.AddNwTsk, "clicking new task");
-		seleniumcore.clickonElement(tasksobjects.ChkBx, "clicking checjbox");
-
-	}
+	
 
 }
