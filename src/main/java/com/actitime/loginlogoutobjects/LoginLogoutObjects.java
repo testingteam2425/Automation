2e7752a1c9b01f1e0909c24ea85cf57491b1c3f3
talla <talla@DESKTOP-MUEO3J6>
2020-05-06 07:59:32 +0530

    Added the annotations on loginlogout class

--- src/main/java/com/actitime/loginlogoutobjects/LoginLogoutObjects.java
+++ src/main/java/com/actitime/loginlogoutobjects/LoginLogoutObjects.java
@@ -1,6 +1,17 @@
 package com.actitime.loginlogoutobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginLogoutObjects {
-	
+
+	@FindBy(xpath="")
+	public WebElement username;
+
+	@FindBy(id="")
+	public WebElement password;
+
+	@FindBy(xpath="")
+	public WebElement loginbutton;
 	
 }
