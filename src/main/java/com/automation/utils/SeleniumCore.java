65929b792e32a62c28c050e3fbf04a7b474db786
talla <talla@DESKTOP-MUEO3J6>
2020-05-06 08:00:24 +0530

    Updated reusability methods

--- src/main/java/com/automation/utils/SeleniumCore.java
+++ src/main/java/com/automation/utils/SeleniumCore.java
@@ -11,8 +11,14 @@ public class SeleniumCore extends BaseClass{
 
 	public void enterText(String locator,String value) {
 		driver.findElement(By.xpath(locator)).sendKeys(value);	
-	
+
 	}
+	
+	public void enterText(String locatortype,String locator,String value) {
+		driver.findElement(By.xpath(locator)).sendKeys(value);	
+
+	}
+	
 	public void clickOnElement(String locator) {
 		driver.findElement(By.xpath(locator)).click();
 	}
@@ -22,6 +28,20 @@ public class SeleniumCore extends BaseClass{
 		Select select=new Select(element);
 		select.selectByVisibleText(visibleText);
 	}
+
+	public void enterText(WebElement locator,String value) {
+		locator.sendKeys(value);
+
+	}
+	public void clickOnElement(WebElement locator) {
+		locator.click();
+	}
+
+	public void SelectVisibleText(WebElement locator,String visibleText) {
+		Select select=new Select(locator);
+		select.selectByVisibleText(visibleText);
+	}
+
 }
 
 
