a7d1a310b37eefb307d5fb7cddfb0635e553cb47
sahitya <sahitya@DESKTOP-I27HIIT>
2020-05-08 17:47:42 +0530

    added mean method with annotation

--- src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
+++ src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
@@ -1,8 +1,14 @@
 package com.actitime.loginlogoututils;
 
+import org.testng.annotations.Test;
+
 import com.automation.base.BaseClass;
 
 public class LoginLogoutUtils extends BaseClass{
+	@Test
+	public void mean() {
+		
+	}
 	
 	
 }
