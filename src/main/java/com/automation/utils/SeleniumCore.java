1dfc3faa0d1881c834b6b22601a7e62b91bebebc
talla <talla@DESKTOP-MUEO3J6>
2020-05-09 08:40:55 +0530

    updated seleniumcore

--- src/main/java/com/automation/utils/SeleniumCore.java
+++ src/main/java/com/automation/utils/SeleniumCore.java
@@ -1,6 +1,5 @@
 package com.automation.utils;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -9,26 +8,7 @@ import com.automation.base.BaseClass;
 
 public class SeleniumCore extends BaseClass{
 
-	public void enterText(String locator,String value) {
-		driver.findElement(By.xpath(locator)).sendKeys(value);	
-
-	}
 	
-	public void enterText(String locatortype,String locator,String value) {
-		driver.findElement(By.xpath(locator)).sendKeys(value);	
-
-	}
-	
-	public void clickOnElement(String locator) {
-		driver.findElement(By.xpath(locator)).click();
-	}
-
-	public void SelectVisibleText(String locator,String visibleText) {
-		WebElement element=driver.findElement(By.xpath(locator));
-		Select select=new Select(element);
-		select.selectByVisibleText(visibleText);
-	}
-
 	public void enterText(WebElement locator,String value) {
 		locator.sendKeys(value);
 
