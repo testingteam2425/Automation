61adbd7b1ad97a2abd371f0b5fb6a691ef54c2e4
talla <talla@DESKTOP-MUEO3J6>
2020-05-09 08:39:53 +0530

    reports

--- src/main/java/com/actitime/reportsobjects/ReportsDashboardObjects.java
+++ src/main/java/com/actitime/reportsobjects/ReportsDashboardObjects.java
@@ -1,19 +1,6 @@
 package com.actitime.reportsobjects;
 
 public class ReportsDashboardObjects {
-	public void report() {
-
-	}
 	
-	public void govind()
-	{}
-	
-	public void raja()
-	{}
-	public void raj1()
-	
-	{
-	
-	}
 	
 }
