ca86fc88dd39bbfe9c400dfdd669e965e132fd41
talla <talla@DESKTOP-MUEO3J6>
2020-05-09 08:42:00 +0530

    updated loginscripts

--- src/test/java/com/actitime/loginlogoutscrips/LoginLogoutScripts.java
+++ src/test/java/com/actitime/loginlogoutscrips/LoginLogoutScripts.java
@@ -1,9 +1,28 @@
 package com.actitime.loginlogoutscrips;
 
+import java.io.IOException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.actitime.loginlogoututils.LoginLogoutUtils;
 import com.automation.base.BaseClass;
 
 public class LoginLogoutScripts extends BaseClass{
-	
-	
 
+	public LoginLogoutUtils loginlogoutUtils;
+
+
+	@Test
+	public void loginPage() throws IOException {
+		initbrowser();
+		loginlogoutUtils.applicationLogin("admin", "manager");
+
+	}
+
+	@BeforeClass
+	public void loading() {
+		loginlogoutUtils=new LoginLogoutUtils();
+
+	}
 }
