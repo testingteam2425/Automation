da7c30fee051be185940db3235b9a43142c791ce
Govind <Govind@DESKTOP-1EHBIHN>
2020-05-10 18:14:32 +0530

    updated

--- src/main/java/com/actitime/reportsobjects/ReportsDashboardObjects.java
+++ src/main/java/com/actitime/reportsobjects/ReportsDashboardObjects.java
@@ -1,6 +1,19 @@
 package com.actitime.reportsobjects;
 
 public class ReportsDashboardObjects {
+	public void report() {
+
+	}
 	
+	public void govind()
+	{}
+	
+	public void raja()
+	{}
+	public void raj1()
+	
+	{
+	
+	}
 	
 }
