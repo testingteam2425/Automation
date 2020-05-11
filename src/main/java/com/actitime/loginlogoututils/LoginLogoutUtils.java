5eca813f2f6ffb99b5228196d20d5b730b4171c8
talla <talla@DESKTOP-MUEO3J6>
2020-05-11 07:10:43 +0530

    loginutils latest commit

--- src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
+++ src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
@@ -4,39 +4,23 @@ import com.actitime.loginlogoutobjects.LoginLogoutObjects;
 import com.automation.base.BaseClass;
 import com.automation.utils.SeleniumCore;
 
-public class LoginLogoutUtils extends BaseClass {
+public class LoginLogoutUtils extends BaseClass{
 
-	public SeleniumCore score;
-	public LoginLogoutObjects loginlogoutobj;
+	public SeleniumCore seleniumCore;
+	public LoginLogoutObjects loginLogoutObjects;
 
-	
-	public LoginLogoutUtils()
-	{
-		score = new SeleniumCore();
-		loginlogoutobj = new LoginLogoutObjects();
-		
+	public LoginLogoutUtils() {
+		seleniumCore=new SeleniumCore();
+		loginLogoutObjects=new LoginLogoutObjects();
 	}
-	
-	
-	
-	public void login(String uuname, String ppwd) {
-			score.entertext(loginlogoutobj.username, uuname);
-		score.entertext(loginlogoutobj.password, ppwd);
-		score.clickonelement(loginlogoutobj.loginbutton);
-	
-	}
-		
-		
+
+
+	public void applicationLogin(String username, String password) {
+		seleniumCore.enterText(loginLogoutObjects.username, username);
+		seleniumCore.enterText(loginLogoutObjects.password, password);
+		seleniumCore.clickOnElement(loginLogoutObjects.loginbutton);
+
 
 	}
 
-	
-
-	
-	
-	
-	
-	
-	
-	
-
+}
