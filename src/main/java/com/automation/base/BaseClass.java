9a412fc4ecf34e353979b059ecc90d240440f9cb
HP <HP@192.168.0.3>
2020-05-12 08:02:43 +0530

    Base updated

--- src/main/java/com/automation/base/BaseClass.java
+++ src/main/java/com/automation/base/BaseClass.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
 
 public class BaseClass {
 
@@ -15,17 +16,18 @@ public class BaseClass {
 	public static Properties testDataProb;
 
 	public static String currentDir = System.getProperty("user.dir");
+	
+	@BeforeSuite
 	public void initbrowser() throws IOException{
 
 		loadConfigFile(currentDir+"\\src\\main\\resources\\config\\config.properties");
-		//loadConfigTestDataFile(currentDir+"\\src\\test\\resources\\config\\testData.properties");
 
 		if (prob.getProperty("Browser").equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\talla\\Downloads\\Drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver",currentDir+"\\src\\main\\resources\\browserDetails\\chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.manage().window().maximize();
 		}else if(prob.getProperty("Browser").equalsIgnoreCase("Firefox")) {
-			System.setProperty("webdriver.gecko.driver","C:\\Users\\talla\\Downloads\\Drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver",currentDir+"/src/main/resources/browserDetails/geckodriver.exe");
 			driver = new FirefoxDriver();
 			driver.manage().window().maximize();
 		}else {
