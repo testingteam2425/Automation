5d7bff4d88240d67f7c63a1692fb2e5fd50f34d0
HP <HP@192.168.0.3>
2020-05-12 08:08:11 +0530

    updated

--- src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
+++ src/main/java/com/actitime/loginlogoututils/LoginLogoutUtils.java
@@ -1,5 +1,7 @@
 package com.actitime.loginlogoututils;
 
+import org.openqa.selenium.support.PageFactory;
+
 import com.actitime.loginlogoutobjects.LoginLogoutObjects;
 import com.automation.base.BaseClass;
 import com.automation.utils.SeleniumCore;
@@ -10,13 +12,15 @@ public class LoginLogoutUtils extends BaseClass{
 	public LoginLogoutObjects loginLogoutObjects;
 
 	public LoginLogoutUtils() {
-		seleniumCore=new SeleniumCore();
-		loginLogoutObjects=new LoginLogoutObjects();
+		seleniumCore =PageFactory.initElements(driver, SeleniumCore.class);
+		loginLogoutObjects=PageFactory.initElements(driver,LoginLogoutObjects.class );
+		
 	}
 
 
-	public void applicationLogin(String username, String password) {
+	public void applicationLogin(String username, String password) throws Exception {
 		seleniumCore.enterText(loginLogoutObjects.username, username);
+		Thread.sleep(3000);
 		seleniumCore.enterText(loginLogoutObjects.password, password);
 		seleniumCore.clickOnElement(loginLogoutObjects.loginbutton);
 
