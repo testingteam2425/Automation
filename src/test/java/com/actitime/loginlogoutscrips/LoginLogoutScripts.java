b38c2167dbe5287b967cdbed9a8001909c37fe2e
rosaiah <r.kakumani@tempbuddy.co>
2020-05-20 08:23:16 +0530

    Updated Login Script

--- src/test/java/com/actitime/loginlogoutscrips/LoginLogoutScripts.java
+++ src/test/java/com/actitime/loginlogoutscrips/LoginLogoutScripts.java
@@ -1,28 +1,34 @@
 package com.actitime.loginlogoutscrips;
 
+import java.util.Map;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.actitime.loginlogoututils.LoginLogoutUtils;
 import com.automation.base.BaseClass;
+import com.automation.utils.ExcelReader;
 
 public class LoginLogoutScripts extends BaseClass{
 
 	public LoginLogoutUtils loginlogoutUtils;
-
-
-	@Test
-	
-	public void loginPage() throws Exception {
-		
-		loginlogoutUtils.applicationLogin("admin", "manager");
-
-	}
-
+	public static ExcelReader reader = ExcelReader.getInstance(currentDir+"\\src\\main\\resources\\TestDataFiles\\TestDataSheet.xls");
+	Map<String, String> testdata;
+ 
 	@BeforeClass
 	public void loading() {
 		loginlogoutUtils=PageFactory.initElements(driver,LoginLogoutUtils.class );
-
+		testdata=reader.getRowValue("ValidCredentilas", "ApplicationLogin");
 	}
+	
+	@Test
+	public void loginPage() throws Exception {
+		String username = testdata.get("username");
+		String password = testdata.get("password");
+		loginlogoutUtils.applicationLogin(username, password);
+		
+	}
+
+	
 }
