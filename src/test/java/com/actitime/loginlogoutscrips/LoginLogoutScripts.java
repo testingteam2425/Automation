82ad1124f386fec23628fb12e569bf47aae1f719
rosaiah <r.kakumani@tempbuddy.co>
2020-05-27 07:53:49 +0530

    Added new invalid method

--- src/test/java/com/actitime/loginlogoutscrips/LoginLogoutScripts.java
+++ src/test/java/com/actitime/loginlogoutscrips/LoginLogoutScripts.java
@@ -15,16 +15,34 @@ public class LoginLogoutScripts extends BaseClass{
 	public LoginLogoutUtils loginlogoutUtils;
 	public static ExcelReader reader = ExcelReader.getInstance(currentDir+"\\src\\main\\resources\\TestDataFiles\\TestDataSheet.xls");
 	Map<String, String> testdata;
+	Map<String, String> testdata1;
  
 	@BeforeClass
 	public void loading() {
 		loginlogoutUtils=PageFactory.initElements(driver,LoginLogoutUtils.class );
 		testdata=reader.getRowValue("ValidCredentials", "ApplicationLogin");
+		testdata1=reader.getRowValue("InvalidCredentials", "ApplicationLogin");
 	}
 	
 	@Test
 	public void loginPage() throws Exception {
 		String username = testdata.get("username");
+		String password = testdata.get("password");
+		loginlogoutUtils.applicationLogin(username, password);
+		
+	}
+	
+	@Test
+	public void inValidPasswordWithLogin() throws Exception {
+		String username = testdata.get("username");
+		String password = testdata1.get("password");
+		loginlogoutUtils.applicationLogin(username, password);
+		
+	}
+	
+	@Test
+	public void inValidUsernameWithLogin() throws Exception {
+		String username = testdata1.get("username");
 		String password = testdata.get("password");
 		loginlogoutUtils.applicationLogin(username, password);
 		
